2980111f39f69afb2a328fc845731dfd0df69838
IlyaPrigogine <prigogine@protonmail.com>
2021-10-21 16:55:36 +0800

    Update upgradeability.spec.ts

--- test-suites/test-aave/upgradeability.spec.ts
+++ test-suites/test-aave/upgradeability.spec.ts
@@ -12,12 +12,22 @@ import {
   getMockVariableDebtToken,
   getStableDebtToken,
   getVariableDebtToken,
+  getFirstSigner,
+  getLendingPoolAddressesProvider,
+  getLendingPoolConfiguratorProxy,
+  getLendingPool,
+  getPriceOracle,
+  getMintableERC20,
+  getIErc20Detailed,
+  getAaveProtocolDataProvider,
 } from '../../helpers/contracts-getters';
 import {
   deployMockAToken,
   deployMockStableDebtToken,
   deployMockVariableDebtToken,
 } from '../../helpers/contracts-deployments';
+import { constants } from 'ethers';
+import { formatEther } from 'ethers/lib/utils';
 
 makeSuite('Upgradeability', (testEnv: TestEnv) => {
   const { CALLER_NOT_POOL_ADMIN } = ProtocolErrors;
@@ -134,6 +144,147 @@ makeSuite('Upgradeability', (testEnv: TestEnv) => {
       ).getIncentivesController()}`
     );
     console.log(`POOL: ${await (await getVariableDebtToken(newVariableTokenAddress)).POOL()}`);
+  });
+
+  it('test getFirstSigner in helpers/contracts-getters.ts', async () => {
+    const { dai, configurator, users } = testEnv;
+    console.log(`getFirstSigner.getAddress(): ${await (await getFirstSigner()).getAddress()}`);
+    console.log(`users[0]: ${users[0].address}`);
+
+    console.log(`getFirstSigner.getChainId(): ${await (await getFirstSigner()).getChainId()}`);
+    console.log(`getFirstSigner.getGasPrice(): ${await (await getFirstSigner()).getGasPrice()}`);
+    console.log(
+      `getFirstSigner.signMessage("hello world"): ${await (
+        await getFirstSigner()
+      ).signMessage('hello world')}`
+    );
+  });
+
+  it('test getLendingPoolAddressesProvider() in contracts-getters.ts, check its value is not equal to [addressZero]', async () => {
+    const { dai, configurator, users } = testEnv;
+    // console.log(`addressProvider: ${await (await getLendingPoolAddressesProvider()).address}`);
+    expect(await getLendingPoolAddressesProvider()).to.be.not.equal(constants.AddressZero);
+  });
+
+  it('test getLendingPoolConfiguratorProxy() in contracts-getters.ts, check its value is not equal to [addressZero]', async () => {
+    console.log(
+      `getLendingPoolConfiguratorProxy: ${await (await getLendingPoolConfiguratorProxy()).address}`
+    );
+    expect(await getLendingPoolConfiguratorProxy()).to.be.not.equal(constants.AddressZero);
+  });
+
+  it('check getLendingPool() in contracts-getters', async () => {
+    const { dai, configurator, users, addressesProvider } = testEnv;
+    const lendingPool = await getLendingPool();
+    console.log(`lendPool.address: ${await lendingPool.address}`);
+    console.log(`lendPool.LENDINGPOOL_REVISION: ${await lendingPool.LENDINGPOOL_REVISION()}`);
+    console.log(`lendPool.getAddressesProvider: ${await lendingPool.getAddressesProvider()}`);
+    console.log(`testEnv.addressesProvider: ${await addressesProvider.address}`);
+    // expect(await lendingPool.getAddressesProvider()).to.be.equal(await addressesProvider.address);
+
+    console.log(
+      `lendingPool.getConfiguration(dai) : ${await lendingPool.getConfiguration(dai.address)}`
+    );
+  });
+
+  it('check getPriceOracle() in contracts-getters', async () => {
+    const { dai, weth, aDai, aWETH, aave, usdc, configurator, users, addressesProvider } = testEnv;
+    const priceOracle = await getPriceOracle();
+    console.log(`priceOracle.address: ${await priceOracle.address}`);
+    console.log(`getEthUsdPrice: ${formatEther(await priceOracle.getEthUsdPrice())}`);
+
+    console.log(`getAssetPrice(dai): ${formatEther(await priceOracle.getAssetPrice(dai.address))}`);
+    console.log(
+      `getAssetPrice(weth.address): ${formatEther(await priceOracle.getAssetPrice(weth.address))}`
+    );
+    console.log(
+      `getAssetPrice(aDai.address): ${formatEther(await priceOracle.getAssetPrice(aDai.address))}`
+    );
+    console.log(
+      `getAssetPrice(aWETH.address): ${formatEther(await priceOracle.getAssetPrice(aWETH.address))}`
+    );
+    console.log(
+      `getAssetPrice(aave.address): ${formatEther(await priceOracle.getAssetPrice(aave.address))}`
+    );
+    console.log(
+      `getAssetPrice(usdc.address): ${formatEther(await priceOracle.getAssetPrice(usdc.address))}`
+    );
+  });
+
+  it('check getMintableERC20(dai.address) in contracts-getters', async () => {
+    const { dai } = testEnv;
+    const tokenInstance = await getMintableERC20(dai.address);
+    expect(await tokenInstance.name()).to.be.equal('DAI');
+    expect(await tokenInstance.symbol()).to.be.equal('DAI');
+    expect(await tokenInstance.decimals()).to.be.equal(18);
+    expect(await tokenInstance.totalSupply()).to.be.equal(0);
+  });
+
+  it('check getMintableERC20(weth.address) in contracts-getters', async () => {
+    const { weth } = testEnv;
+    const tokenInstance = await getMintableERC20(weth.address);
+    expect(await tokenInstance.name()).to.be.equal('Wrapped Ether');
+    expect(await tokenInstance.symbol()).to.be.equal('WETH');
+    expect(await tokenInstance.decimals()).to.be.equal(18);
+    expect(await tokenInstance.totalSupply()).to.be.equal(0);
+  });
+
+  it('check getMintableERC20(aave.address) in contracts-getters', async () => {
+    const { aave } = testEnv;
+    const tokenInstance = await getMintableERC20(aave.address);
+    expect(await tokenInstance.name()).to.be.equal('AAVE');
+    expect(await tokenInstance.symbol()).to.be.equal('AAVE');
+    expect(await tokenInstance.decimals()).to.be.equal(18);
+    expect(await tokenInstance.totalSupply()).to.be.equal(0);
+  });
+
+  it('check getMintableERC20(usdc.address) in contracts-getters', async () => {
+    const { usdc } = testEnv;
+    const tokenInstance = await getMintableERC20(usdc.address);
+    expect(await tokenInstance.name()).to.be.equal('USDC');
+    expect(await tokenInstance.symbol()).to.be.equal('USDC');
+    expect(await tokenInstance.decimals()).to.be.equal(6);
+    expect(await tokenInstance.totalSupply()).to.be.equal(0);
+  });
+
+  it('check getIErc20Detailed(dai.address) in contracts-getters', async () => {
+    const { dai } = testEnv;
+    const tokenInstance = await getIErc20Detailed(dai.address);
+    expect(await tokenInstance.name()).to.be.equal('DAI');
+    expect(await tokenInstance.symbol()).to.be.equal('DAI');
+    expect(await tokenInstance.decimals()).to.be.equal(18);
+    expect(await tokenInstance.totalSupply()).to.be.equal(0);
+  });
+
+  it('check getIErc20Detailed(aDai.address) in contracts-getters', async () => {
+    const { aDai } = testEnv;
+    const tokenInstance = await getIErc20Detailed(aDai.address);
+    // console.log(`name: ${await tokenInstance.name()}`);
+    // console.log(`symbol: ${await tokenInstance.symbol()}`);
+
+    expect(await tokenInstance.name()).to.be.equal('Aave interest bearing DAI');
+    expect(await tokenInstance.symbol()).to.be.equal('aDAI');
+    expect(await tokenInstance.decimals()).to.be.equal(18);
+    expect(await tokenInstance.totalSupply()).to.be.equal(0);
+  });
+
+  it('check getIErc20Detailed(aWETH.address) in contracts-getters', async () => {
+    const { aWETH } = testEnv;
+    const tokenInstance = await getIErc20Detailed(aWETH.address);
+    expect(await tokenInstance.name()).to.be.equal('Aave interest bearing WETH');
+    expect(await tokenInstance.symbol()).to.be.equal('aWETH');
+    expect(await tokenInstance.decimals()).to.be.equal(18);
+    expect(await tokenInstance.totalSupply()).to.be.equal(0);
+  });
+
+  it('check getAaveProtocolDataProvider() in contracts-getters', async () => {
+    const { dai } = testEnv;
+    const aaveDP = await getAaveProtocolDataProvider();
+
+    expect(await aaveDP.getAllATokens()).to.be.not.equal(null);
+    expect(await aaveDP.getAllReservesTokens()).to.be.not.equal(null);
+    expect(await aaveDP.getReserveData(dai.address)).to.be.not.equal(null);
+    expect(await aaveDP.getReserveTokensAddresses(dai.address)).to.be.not.equal(null);
   });
 
   it('Tries to update the DAI Atoken implementation with a different address than the lendingPoolManager', async () => {
