7286e3e9a9b57b7a3ac7c8845439ce2f18844dc6
IlyaPrigogine <prigogine@protonmail.com>
2021-10-22 16:22:18 +0800

    Update upgradeability.spec.ts

--- test-suites/test-aave/upgradeability.spec.ts
+++ test-suites/test-aave/upgradeability.spec.ts
@@ -20,6 +20,19 @@ import {
   getMintableERC20,
   getIErc20Detailed,
   getAaveProtocolDataProvider,
+  getInterestRateStrategy,
+  getMockFlashLoanReceiver,
+  getLendingRateOracle,
+  getAllMockedTokens,
+  getReserveLogic,
+  getGenericLogic,
+  getStableAndVariableTokensHelper,
+  getATokensAndRatesHelper,
+  getWETHGateway,
+  getWETHMocked,
+  getMockAToken,
+  getSelfdestructTransferMock,
+  getLendingPoolImpl,
 } from '../../helpers/contracts-getters';
 import {
   deployMockAToken,
@@ -28,6 +41,7 @@ import {
 } from '../../helpers/contracts-deployments';
 import { constants } from 'ethers';
 import { formatEther } from 'ethers/lib/utils';
+import BigNumber from 'bignumber.js';
 
 makeSuite('Upgradeability', (testEnv: TestEnv) => {
   const { CALLER_NOT_POOL_ADMIN } = ProtocolErrors;
@@ -287,6 +301,202 @@ makeSuite('Upgradeability', (testEnv: TestEnv) => {
     expect(await aaveDP.getReserveTokensAddresses(dai.address)).to.be.not.equal(null);
   });
 
+  it('check getMockFlashLoanReceiver() in contracts-getters', async () => {
+    const { dai } = testEnv;
+    const mockFlashLR = await getMockFlashLoanReceiver();
+    console.log(`address: ${await mockFlashLR.address}`);
+    console.log(`amountToApprove: ${await mockFlashLR.amountToApprove()}`);
+    console.log(`simulateEOA: ${await mockFlashLR.simulateEOA()}`);
+    console.log(`LENDING_POOL: ${await mockFlashLR.LENDING_POOL()}`);
+  });
+
+  it('check getInterestRateStrategy() in contracts-getters', async () => {
+    const interestRS = await getInterestRateStrategy();
+    expect(await interestRS.baseVariableBorrowRate()).to.be.equal(0);
+    expect(formatEther(await interestRS.getMaxVariableBorrowRate()).toString()).to.be.equal(
+      '3070000000.0'
+    );
+    expect(formatEther(await interestRS.stableRateSlope1()).toString()).to.be.equal('0.0');
+    expect(formatEther(await interestRS.stableRateSlope2()).toString()).to.be.equal('0.0');
+    expect(formatEther(await interestRS.variableRateSlope1()).toString()).to.be.equal('70000000.0');
+    expect(formatEther(await interestRS.variableRateSlope2()).toString()).to.be.equal(
+      '3000000000.0'
+    );
+  });
+
+  it('check getLendingRateOracle() in contracts-getters', async () => {
+    const { dai, weth } = testEnv;
+    const lendingRO = await getLendingRateOracle();
+    expect(formatEther(await lendingRO.getMarketBorrowRate(dai.address)).toString()).to.be.equal(
+      '39000000.0'
+    );
+    expect(formatEther(await lendingRO.getMarketLiquidityRate(dai.address)).toString()).to.be.equal(
+      '0.0'
+    );
+    expect(formatEther(await lendingRO.getMarketBorrowRate(weth.address)).toString()).to.be.equal(
+      '30000000.0'
+    );
+    expect(
+      formatEther(await lendingRO.getMarketLiquidityRate(weth.address)).toString()
+    ).to.be.equal('0.0');
+
+    // console.log(`dai.marketBorrowRate: ${await lendingRO.getMarketBorrowRate(dai.address)}`);
+    // console.log(`dai.marketLiquidityRate: ${await lendingRO.getMarketLiquidityRate(dai.address)}`);
+    // console.log(`weth.marketBorrowRate: ${await lendingRO.getMarketBorrowRate(weth.address)}`);
+    // console.log(`weth.marketLiquidityRate: ${await lendingRO.getMarketLiquidityRate(weth.address)}`);
+  });
+
+  it('check getAllMockedTokens() in contracts-getters', async () => {
+    const allMT = await getAllMockedTokens();
+    // console.log(`${await allMT["DAI"].name()}`);
+    // console.log(`${await allMT["DAI"].symbol()}`);
+    // console.log(`${await allMT["DAI"].decimals()}`);
+    // console.log(`${await allMT["DAI"].totalSupply()}`);
+
+    expect(await allMT['DAI'].name()).to.be.equal('DAI');
+    expect(await allMT['DAI'].symbol()).to.be.equal('DAI');
+    expect(await allMT['DAI'].decimals()).to.be.equal(18);
+    expect(await allMT['DAI'].totalSupply()).to.be.equal(0);
+
+    expect(await allMT['WETH'].name()).to.be.equal('Wrapped Ether');
+    expect(await allMT['WETH'].symbol()).to.be.equal('WETH');
+    expect(await allMT['WETH'].decimals()).to.be.equal(18);
+    expect(await allMT['WETH'].totalSupply()).to.be.equal(0);
+  });
+
+  it('check getReserveLogic() in contracts-getters', async () => {
+    const reserveLogic = await getReserveLogic();
+    console.log(`reserveLogic.address: ${await reserveLogic.address}`);
+  });
+
+  it('check getGenericLogic() in contracts-getters', async () => {
+    const genericLogic = await getGenericLogic();
+    console.log(`address: ${await genericLogic.address}`);
+    console.log(
+      `HEALTH_FACTOR_LIQUIDATION_THRESHOLD: ${await genericLogic.HEALTH_FACTOR_LIQUIDATION_THRESHOLD()}`
+    );
+    expect(
+      formatEther(await genericLogic.HEALTH_FACTOR_LIQUIDATION_THRESHOLD()).toString()
+    ).to.be.equal('1.0');
+  });
+
+  it('check getStableAndVariableTokensHelper() in contracts-getters', async () => {
+    const stableAVTH = await getStableAndVariableTokensHelper();
+    console.log(`address: ${stableAVTH.address}`);
+    console.log(`owner: ${await stableAVTH.owner()}`);
+  });
+
+  it('check getATokensAndRatesHelper in contracts-getters', async () => {
+    const atokenRH = await getATokensAndRatesHelper();
+    console.log(`address: ${await atokenRH.address}`);
+    console.log(`owner: ${await atokenRH.owner()}`);
+  });
+
+  it('check getWETHGateway in contracts-getters', async () => {
+    const { weth } = testEnv;
+    const wethG = await getWETHGateway();
+    console.log(`address: ${await wethG.address}`);
+    console.log(`owner: ${await wethG.owner()}`);
+    console.log(`getWETHGateway: ${await wethG.getWETHAddress()}`);
+    expect(await wethG.getWETHAddress()).to.be.equal(weth.address);
+  });
+
+  it('check getWETHMocked in contracts-getters', async () => {
+    const { weth } = testEnv;
+    console.log(`testEnv.weth: ${await weth.address}`);
+
+    const wethM = await getWETHMocked();
+    expect(await wethM.address).to.be.equal(weth.address);
+    expect(await wethM.name()).to.be.equal('Wrapped Ether');
+    expect(await wethM.symbol()).to.be.equal('WETH');
+    expect(await wethM.decimals()).to.be.equal(18);
+    expect(await wethM.totalSupply()).to.be.equal(0);
+
+    console.log(`address: ${await wethM.address}`);
+    console.log(`name: ${await wethM.name()}`);
+    console.log(`symbol: ${await wethM.symbol()}`);
+    console.log(`decimals: ${await wethM.decimals()}`);
+    console.log(`totalSupply: ${await wethM.totalSupply()}`);
+  });
+
+  it('check getMockAToken in contracts-getters', async () => {
+    const mAToken = await getMockAToken();
+
+    console.log(`address: ${await mAToken.address}`);
+    console.log(`name: ${await mAToken.name()}`);
+    console.log(`symbol: ${await mAToken.symbol()}`);
+    console.log(`decimals: ${await mAToken.decimals()}`);
+    console.log(`totalSupply: ${await mAToken.totalSupply()}`);
+
+    expect(await mAToken.name()).to.be.equal('Aave Interest bearing DAI updated');
+    expect(await mAToken.symbol()).to.be.equal('aDAI');
+    expect(await mAToken.decimals()).to.be.equal(18);
+    expect(await mAToken.totalSupply()).to.be.equal(0);
+  });
+
+  it('check getMockAToken(aDai.address) in contracts-getters', async () => {
+    const { dai, aDai } = testEnv;
+    const mAToken = await getMockAToken(aDai.address);
+
+    // console.log(`address: ${await mAToken.address}`);
+    // console.log(`name: ${await mAToken.name()}`);
+    // console.log(`symbol: ${await mAToken.symbol()}`);
+    // console.log(`decimals: ${await mAToken.decimals()}`);
+    // console.log(`totalSupply: ${await mAToken.totalSupply()}`);
+
+    expect(await mAToken.name()).to.be.equal('Aave interest bearing DAI');
+    expect(await mAToken.symbol()).to.be.equal('aDAI');
+    expect(await mAToken.decimals()).to.be.equal(18);
+    expect(await mAToken.totalSupply()).to.be.equal(0);
+  });
+
+  it('check getMockAToken(aWETH.address) in contracts-getters', async () => {
+    const { aDai, aWETH } = testEnv;
+    const mAToken = await getMockAToken(aWETH.address);
+
+    // console.log(`address: ${await mAToken.address}`);
+    // console.log(`name: ${await mAToken.name()}`);
+    // console.log(`symbol: ${await mAToken.symbol()}`);
+    // console.log(`decimals: ${await mAToken.decimals()}`);
+    // console.log(`totalSupply: ${await mAToken.totalSupply()}`);
+
+    expect(await mAToken.name()).to.be.equal('Aave interest bearing WETH');
+    expect(await mAToken.symbol()).to.be.equal('aWETH');
+    expect(await mAToken.decimals()).to.be.equal(18);
+    expect(await mAToken.totalSupply()).to.be.equal(0);
+  });
+
+  it('check getMockVariableDebtToken() in contracts-getters', async () => {
+    const mVDT = await getMockVariableDebtToken();
+    // console.log(`name: ${await mVDT.name()}`);
+    // console.log(`symbol: ${await mVDT.symbol()}`);
+    // console.log(`decimals: ${await mVDT.decimals()}`);
+    // console.log(`totalSupply: ${await mVDT.totalSupply()}`);
+
+    expect(await mVDT.name()).to.be.equal('Aave variable debt bearing DAI updated');
+    expect(await mVDT.symbol()).to.be.equal('variableDebtDAI');
+    expect(await mVDT.decimals()).to.be.equal(18);
+    expect(await mVDT.totalSupply()).to.be.equal(0);
+  });
+
+  it('check getMockStableDebtToken() in contracts-getters', async () => {
+    const mSDT = await getMockStableDebtToken();
+    // console.log(`name: ${await mSDT.name()}`);
+    // console.log(`symbol: ${await mSDT.symbol()}`);
+    // console.log(`decimals: ${await mSDT.decimals()}`);
+    // console.log(`totalSupply: ${await mSDT.totalSupply()}`);
+
+    expect(await mSDT.name()).to.be.equal('Aave stable debt bearing DAI updated');
+    expect(await mSDT.symbol()).to.be.equal('stableDebtDAI');
+    expect(await mSDT.decimals()).to.be.equal(18);
+    expect(await mSDT.totalSupply()).to.be.equal(0);
+  });
+
+  it('check getSelfdestructTransferMock() in contracts-getters', async () => {
+    const sTM = await getSelfdestructTransferMock();
+    console.log(`address: ${await sTM.address}`);
+  });
+
   it('Tries to update the DAI Atoken implementation with a different address than the lendingPoolManager', async () => {
     const { dai, configurator, users } = testEnv;
 
@@ -466,5 +676,62 @@ makeSuite('Upgradeability', (testEnv: TestEnv) => {
     const tokenName = await debtToken.name();
 
     expect(tokenName).to.be.eq('Aave variable debt bearing DAI updated', 'Invalid token name');
+  });
+
+  // new test case
+  it('check getSelfdestructTransferMock() in contracts-getters', async () => {
+    const sTM = await getSelfdestructTransferMock();
+    // console.log(`address: ${await sTM.address}`);
+    expect(await sTM.address).to.be.not.equal(constants.AddressZero);
+  });
+
+  it('check getLendingPoolImpl.getReserveData in contracts-getters', async () => {
+    const { dai } = testEnv;
+    const lendingPI = await getLendingPoolImpl();
+    const rD = await lendingPI.getReserveData(dai.address);
+    // console.log(`aTokenAddress: ${await reserveData.aTokenAddress}`);
+    // console.log(`data: ${await reserveData.configuration.data}`)
+
+    expect(await rD.aTokenAddress).to.be.equal(constants.AddressZero);
+    expect(await rD.configuration.data).to.be.equal(0);
+
+    // console.log(`currentLiquidityRate: ${formatEther(await rD.currentLiquidityRate)}`);
+    // console.log(`currentStableBorrowRate ${formatEther(await rD.currentStableBorrowRate)}`);
+    // console.log(`currentVariableBorrowRate ${formatEther(await rD.currentVariableBorrowRate)}`);
+    // console.log(`interestRateStrategyAddress ${await rD.interestRateStrategyAddress}`);
+    // console.log(`lastUpdateTimestamp ${await rD.lastUpdateTimestamp}`);
+    // console.log(`liquidityIndex ${await rD.liquidityIndex}`);
+    // console.log(`stableDebtTokenAddress ${await rD.stableDebtTokenAddress}`);
+    // console.log(`variableBorrowIndex ${await rD.variableBorrowIndex}`);
+    // console.log(`variableDebtTokenAddress ${await rD.variableDebtTokenAddress}`);
+
+    expect(await rD.currentLiquidityRate).to.be.equal(0);
+    expect(await rD.currentStableBorrowRate).to.be.equal(0);
+    expect(await rD.currentVariableBorrowRate).to.be.equal(0);
+    expect(await rD.interestRateStrategyAddress).to.be.equal(constants.AddressZero);
+    expect(await rD.lastUpdateTimestamp).to.be.equal(0);
+    expect(await rD.liquidityIndex).to.be.equal(0);
+    expect(await rD.stableDebtTokenAddress).to.be.equal(constants.AddressZero);
+    expect(await rD.variableBorrowIndex).to.be.equal(0);
+    expect(await rD.variableDebtTokenAddress).to.be.equal(constants.AddressZero);
+  });
+
+  it('check getLendingPoolImpl() in contracts-getters', async () => {
+    const { dai, users } = testEnv;
+    const lendingPI = await getLendingPoolImpl();
+    // console.log(`address: ${await lendingPI.address}`);
+    // console.log(`getAddressesProvider: ${await lendingPI.getAddressesProvider()}`);
+    // console.log(`dai.configration.data: ${await (await lendingPI.getConfiguration(dai.address)).data}`);
+
+    expect(await lendingPI.address).to.be.not.equal(constants.AddressZero);
+    expect(await lendingPI.getAddressesProvider()).to.be.equal(constants.AddressZero);
+    expect(await (await lendingPI.getConfiguration(dai.address)).data).to.be.equal(0);
+
+    console.log(
+      `getReserveNormalizedIncome: ${await lendingPI.getReserveNormalizedIncome(dai.address)}`
+    );
+    console.log(`getReservesList: ${await lendingPI.getReservesList()}`);
+    // console.log(`getUserAccountData: ${await lendingPI.getUserAccountData(users[1].address)}`);
+    console.log(`${await lendingPI.getReserveNormalizedIncome(dai.address)}`);
   });
 });
